94894a2eb729eded6578414049eb826d36c63175
Alexander <nefest71@gmail.com>
2023-06-23 20:46:11 +0300

    Первоначальная настройка проекта
    * Удален main сториборд и инициализировано создание окна кодом
    * Добавлены ассеты для светлой и темной темы

---
+++ GitHubFollowers/GitHubFollowers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Alexander Vagin on 23.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
+
