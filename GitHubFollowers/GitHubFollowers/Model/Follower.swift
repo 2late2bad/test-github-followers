b29b5654b9e837d2b9eac70fdb521ac1d40c3dcf
Alexander <nefest71@gmail.com>
2023-06-26 14:40:28 +0300

    Создание моделей структур для работы с API GitHub

---
+++ GitHubFollowers/GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Alexander Vagin on 26.06.2023.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
