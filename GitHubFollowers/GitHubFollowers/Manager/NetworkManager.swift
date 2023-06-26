ff152c4955f4674573615f90b5766b9fdf2146fa
Alexander <nefest71@gmail.com>
2023-06-27 00:58:48 +0300

    Загрузка из сети аватаров и их кеширование

--- GitHubFollowers/GitHubFollowers/Manager/NetworkManager.swift
+++ GitHubFollowers/GitHubFollowers/Manager/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Alexander Vagin on 26.06.2023.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     
-    static let shared = NetworkManager()
-    let baseURL       = "https://api.github.com/"
+    static let shared   = NetworkManager()
+    private let baseURL = "https://api.github.com/"
+    let cache           = NSCache<NSString, UIImage>()
     
     private init() {}
     
