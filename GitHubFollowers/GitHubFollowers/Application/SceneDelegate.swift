a75471f390e38c228eda8f765d72c0a995b688af
Alexander <nefest71@gmail.com>
2023-06-27 16:18:22 +0300

    Функционал поиска по найденным элементам

--- GitHubFollowers/GitHubFollowers/Application/SceneDelegate.swift
+++ GitHubFollowers/GitHubFollowers/Application/SceneDelegate.swift
@@ -41,6 +41,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func createTabBar() -> UITabBarController {
         let tabBar                      = UITabBarController()
         UITabBar.appearance().tintColor = .systemGreen
+        
+//        if #available(iOS 13.0, *) {
+//            let tabBarAppearance: UITabBarAppearance = UITabBarAppearance()
+//            tabBarAppearance.configureWithDefaultBackground()
+//            tabBarAppearance.backgroundColor = .systemBackground
+//            UITabBar.appearance().standardAppearance = tabBarAppearance
+//
+//            if #available(iOS 15.0, *) {
+//                UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+//            }
+//        }
+        
         tabBar.viewControllers          = [createSearchNC(), createFavoritesNC()]
         return tabBar
     }
