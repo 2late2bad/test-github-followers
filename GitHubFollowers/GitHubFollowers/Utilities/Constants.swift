5a567ecf5be9eef9851e02005ead823539039b54
Alexander <nefest71@gmail.com>
2023-06-27 18:14:59 +0300

    Добавлен childVC для UserInfoHeaderVC

---
+++ GitHubFollowers/GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Alexander Vagin on 27.06.2023.
+//
+
+import Foundation
+
+typealias C = Constants
+
+enum Constants {
+    
+    enum SFSymbols {
+        static let location = "mappin.and.ellipse"
+    }
+}
