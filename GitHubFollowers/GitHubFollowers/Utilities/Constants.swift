032d069333db44f5fca46a0a99a8b524cca6e270
Alexander <nefest71@gmail.com>
2023-06-27 22:20:59 +0300

    Создание ItemInfoView

--- GitHubFollowers/GitHubFollowers/Utilities/Constants.swift
+++ GitHubFollowers/GitHubFollowers/Utilities/Constants.swift
@@ -12,6 +12,10 @@ typealias C = Constants
 enum Constants {
     
     enum SFSymbols {
-        static let location = "mappin.and.ellipse"
+        static let location  = "mappin.and.ellipse"
+        static let repos     = "folder"
+        static let gists     = "text.alignleft"
+        static let followers = "heart"
+        static let following = "person.2"
     }
 }
